39cf1042327931ee0e38be1585b22c5a975b6d9b
meh <meh@schizofreni.co>
2014-03-05 12:00:23 +0100

    parser: rewrite parser to be lazy

--- lib/cesso/parser.ex
+++ lib/cesso/parser.ex
@@ -7,68 +7,83 @@
 # 0. You just DO WHAT THE FUCK YOU WANT TO.
 
 defmodule Cesso.Parser do
-  defrecordp :options, separator: nil
+  def parse(source, options \\ [])
 
-  def parse(string, options // []) do
-    separator = case options[:separator] do
-      nil ->
-        ?,
-
-      << sep :: utf8 >> ->
-        sep
-
-      sep when sep |> is_integer ->
-        sep
-    end
-
-    rows(string) |> Stream.map &columns(&1, options(separator: separator))
+  def parse(string, options) when string |> is_binary do
+    parse(lines(string), options)
   end
 
-  defp rows(string) do
+  def parse(lines, options) do
+    separator = options[:separator] || ","
+
+    Stream.map unescape(lines), fn line ->
+      columns(line, separator)
+    end
+  end
+
+  defp lines(string) do
     Stream.unfold string, fn string ->
-      case string |> :binary.split ["\r\n", "\n"] do
-        [""] ->
-          nil
-
-        [left] ->
-          { left, "" }
-
-        [left, right] ->
-          { left, right }
+      case string |> String.split(["\r\n", "\n"], global: false) do
+        [""]         -> nil
+        [elem, rest] -> { elem, rest }
+        [elem]       -> { elem, "" }
       end
     end
   end
 
-  defp columns("", _) do
-    []
-  end
+  defp unescape(lines) do
+    Stream.transform lines, { [], false }, fn line, { current, inside } ->
+      current = [line | current]
 
-  defp columns(line, options) do
-    columns([], line, options) |> :lists.reverse
-  end
-
-  defp columns(acc, line, options(separator: separator) = options) do
-    case until("", line, separator) do
-      { element, "" } ->
-        [element |> nillify | acc]
-
-      { element, rest } ->
-        [element |> nillify | acc] |> columns(rest, options)
+      if closed?(line, inside) do
+        { [current |> Enum.reverse |> Enum.join("\n")], { [], false } }
+      else
+        { [], { current, true } }
+      end
     end
   end
 
-  defp until(acc, "", _) do
-    { acc, "" }
+  defp closed?(<< ?" :: utf8, rest :: binary >>, inside) do
+    closed?(rest, not inside)
   end
 
-  defp until(acc, << separator :: 8, rest :: binary >>, separator) do
-    { acc, rest }
+  defp closed?(<< _ :: utf8, rest :: binary >>, inside) do
+    closed?(rest, inside)
   end
 
-  defp until(acc, << ch :: 8, rest :: binary >>, separator) do
-    << acc :: binary, ch :: 8 >> |> until(rest, separator)
+  defp closed?("", inside) do
+    not inside
   end
 
-  defp nillify(""), do: nil
-  defp nillify(v),  do: v
+  defp columns(line, separator) do
+    columns([], false, 0, line, line, separator) |> Enum.reverse
+      |> Enum.map fn
+        << ?" :: utf8, rest :: binary >> ->
+          String.slice(rest, 0 .. -2) |> String.replace "\"\"", "\""
+
+        column ->
+          column
+      end
+  end
+
+  defp columns(columns, _, offset, "", line, _) do
+    [String.slice(line, 0 .. offset) | columns]
+  end
+
+  defp columns(columns, inside, offset, << ?" :: utf8, rest :: binary >>, line, separator) do
+    columns(columns, not inside, offset + 1, rest, line, separator)
+  end
+
+  defp columns(columns, inside, offset, rest, line, separator) do
+    if not inside and rest |> String.starts_with? separator do
+      columns = [String.slice(line, 0 .. offset - 1) | columns]
+      line    = String.slice(line, offset + String.length(separator) .. -1)
+
+      columns(columns, inside, 0, line, line, separator)
+    else
+      << _ :: utf8, rest :: binary >> = rest
+
+      columns(columns, inside, offset + 1, rest, line, separator)
+    end
+  end
 end
