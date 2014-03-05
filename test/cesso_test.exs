f8926516061cc5814b4f8ee73d514da247c2dd2e
meh <meh@schizofreni.co>
2014-03-05 12:15:36 +0100

    test: add some decode options tests

---
+++ test/cesso_test.exs
@@ -0,0 +1,19 @@
+defmodule CessoTest do
+  use ExUnit.Case
+  alias Cesso, as: C
+  import Enum, only: [to_list: 1]
+
+  test "decodes with columns" do
+    res = C.decode(~s<lol,wut>, columns: [:a, :b]) |> Enum.at(0)
+
+    assert res[:a] == "lol"
+    assert res[:b] == "wut"
+  end
+
+  test "decodes with header column" do
+    res = C.decode(~s<a,b\nlol,wut>, columns: true) |> Enum.at(0)
+
+    assert res["a"] == "lol"
+    assert res["b"] == "wut"
+  end
+end
